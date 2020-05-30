749c281b0c0d16986ac7da95f441c53f22f0209d
张应颖 <zhangyingying@jianbing.com>
2020-05-30 14:27:12 +0800

    更新命令行

--- src/getBranchInfo.js
+++ src/getBranchInfo.js
@@ -2,8 +2,9 @@ const child_process = require('child_process')
 const log = require('../tools/log')
 
 const getBranchInfo = ()=> {
-  let branch; // 所有分支
-  let branchList; // 所有分支列表
+  // 获取所有分支
+  let branch;
+  let branchList
   try {
     branch = child_process.execSync('git branch -a', { encoding: 'utf8' });
     branchList = branch.split('\n').map(item => item.trim())
@@ -11,11 +12,21 @@ const getBranchInfo = ()=> {
     currentBranch = currentBranch ? currentBranch.substr(2, currentBranch.length - 1) : ''
     return {
       branchList,
-      currentBranch // 当前分支
+      currentBranch
     }
   } catch (e) {
-    log.error('\n ERROR ', 'git branch -a 执行错误，可能没有关联到远程仓库')
-    log('您可以尝试命令: git remote add origin [url]\n')
+    const hasRemote = child_process.execSync('git remote', { encoding: 'utf8' });
+    if(hasRemote) {
+      log.error(' ERROR ', '远程仓库还没有您的提交信息，请先push')
+			log('您可以尝试命令: git pull origin [branch(?master)]\n')
+			log('OR')
+			log('您可以尝试命令: git push --set-upstream origin [branch(?master)]\n')
+    } else {
+			log.error('\n ERROR ', 'git branch -a 执行错误，可能没有关联到远程仓库')
+      log('您可以尝试命令: git remote add origin [url]\n')
+    }
+    
+    
     process.exit()
   }
 }
